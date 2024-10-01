f488e64a42f76f416ab6ec22f0169c93b865f646
RITNana <ns8363@g.rit.edu>
2024-10-01 19:44:19 -0400

    HOOTS

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const generateNewId = () => crypto.randomUUID();
+
+const hoots = [{
+  id: generateNewId(),
+  content: "Let's Rock!",
+  createdAt: new Date(),
+}];
+
+router.get('/hoots', (req, res) => {
+  res.json(hoots);
+});
+
 // send a request to get the data for our helloJSON request
 router.get('/helloJSON', (req, res) => {
   res.status(200).json({ message: 'Hello There!' });
